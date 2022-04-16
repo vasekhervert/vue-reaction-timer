96b6e3a51c14073bb2725d14547f71caf578c8af
vasekhervert <vasekhervert@gmail.com>
2022-04-16 11:43:48 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start">Start</button>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+return {
+  isPlaying: false,
+  delay: null
+}
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
