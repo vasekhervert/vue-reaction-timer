a99d991727ffbd036b9bfc1f4b2a3288f61969b6
vasekhervert <vasekhervert@gmail.com>
2022-04-17 20:10:11 +0200

    finished game

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-  <button @click="start">Start</button>
+  <button @click="start" :disabled='isPlaying'>Start</button>
+  <Block v-if='isPlaying' :delay='delay' @end='endGame' />
+  <Results v-if='showResults' :score='score' />
 </template>
 
 <script>
 
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Block,
+    Results
   },
   data() {
-return {
-  isPlaying: false,
-  delay: null
-}
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-      console.log(this.delay);
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
@@ -35,5 +47,20 @@ return {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">Rank: {{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Superb'
+        } else if (this.score < 400) {
+            this.rank = 'Rapidfire'
+        } else {
+            this.rank = 'Snail pace..'
+        }
+    }
+}
+</script>
+
+<style>
+ .rank {
+     font-size: 1.4em;
+     color: #0faf87;
+     font-weight: 700;
+ }
+</style>
